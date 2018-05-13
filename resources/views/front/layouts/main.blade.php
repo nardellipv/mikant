1e0f3da39a8990b176857b9ba1e3a398030fc78a
pa3lo <nardellipv@gmail.com>
2018-05-13 19:01:52 -0300

    fixed

--- resources/views/front/layouts/main.blade.php
+++ resources/views/front/layouts/main.blade.php
@@ -3,7 +3,7 @@
 <head>
     <title>MikAnt | Desarrollo de software</title>
     <link href="{{ asset('css/bootstrap.css') }}" rel='stylesheet' type='text/css'/>
-    <link rel="canonical" href="http://www.mikant.com" />
+    <link rel="canonical" href="http://www.mikant.com"/>
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="{{ asset('js/jquery.min.js') }}"></script>
     <!-- Custom Theme files -->
@@ -11,10 +11,16 @@
     <link href="{{ asset ('css/topStyle.css') }}" rel="stylesheet" type="text/css" media="all"/>
     <link rel="icon" href="{{ asset('favicon.ico') }}">
     <!-- Custom Theme files -->
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-    <meta name="keywords" content="United Comms Responsive web template, Bootstrap Web Templates, Flat Web Templates, Andriod Compatible web template,
-Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyErricsson, Motorola web design"/>
+    <meta name="title" content="Desarrollo de sistemas web">
+    <meta name="description" content="Nos dedicamos al desarrollo y diseño de sistemas web autoadministrables. Creación de sistemas web para el manejo de
+    empresas">
+    <meta name="keywords"
+          content="diseño web, diseño web argentina, empresa desarrollo web, empresa diseño web, empresa dedicada al desarrollo de paginas web,  empresas desarrollo paginas web, los mejos sistemas webs, aplicaciones web, aplicaciones  administrables web">
+    <meta name="robots" content="index, follow">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="language" content="Spanish">
+    <meta name="revisit-after" content="1 days">
+    <meta name="author" content="MikAnt">
     <script type="text/javascript" src="{{ asset('js/top.js') }}"></script>
     <script type="application/x-javascript"> addEventListener("load", function () {
             setTimeout(hideURLbar, 0);
@@ -27,15 +33,28 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
     <link href='http://fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic,900,900italic'
           rel='stylesheet' type='text/css'>
 
-    @include('front.external.analytics')
+@include('front.external.analytics')
 
-    @include('front.external.drift')
+@include('front.external.drift')
 
-    @include('front.external.oneSignal')
+@include('front.external.oneSignal')
+
+<!-- Hotjar Tracking Code for mikant.com -->
+    <script>
+        (function(h,o,t,j,a,r){
+            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+            h._hjSettings={hjid:878852,hjsv:6};
+            a=o.getElementsByTagName('head')[0];
+            r=o.createElement('script');r.async=1;
+            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+            a.appendChild(r);
+        })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+    </script>
 
 </head>
 <body>
-<button onclick="topFunction()" id="myBtn" title="Go to top"><img src="{{ asset('images/arr.png') }}" alt="flecha"></button>
+<button onclick="topFunction()" id="myBtn" title="Go to top"><img src="{{ asset('images/arr.png') }}" alt="flecha">
+</button>
 
 @include('front.layouts.parts.message_success')
 
@@ -69,16 +88,16 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 
 {{--desplazamiento al hacer click en el menu--}}
 <script>
-    $(function(){
+    $(function () {
 
-        $('a[href*=#]').click(function() {
+        $('a[href*=#]').click(function () {
 
-            if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
                 && location.hostname == this.hostname) {
 
                 var $target = $(this.hash);
 
-                $target = $target.length && $target || $('[name=' + this.hash.slice(1) +']');
+                $target = $target.length && $target || $('[name=' + this.hash.slice(1) + ']');
 
                 if ($target.length) {
 
--- resources/views/front/layouts/parts/banner.blade.php
+++ resources/views/front/layouts/parts/banner.blade.php
@@ -2,9 +2,9 @@
     <div class="container">
         <div class="banner-info">
             <h1>Desarrollo web a medida</h1>
-            <h2>Mostra tu empresa al mundo, con diseños exclusivos y adaptado a los que necesitas.</h2>
-            <p>Creamos aplicaciones web diseñadas unicamente a medida, totalmente diseñada por nostros y
-            para nuestros clientes.</p>
+            <h2>Mostra tu empresa al mundo, con diseños exclusivos y adaptado a lo que necesitas.</h2>
+            <p>Creamos aplicaciones y sitios web autoadministrables diseñadas a medida para nuestros clientes
+            y para lo que ellos necesitan.</p>
         </div>
     </div>
 </div>
